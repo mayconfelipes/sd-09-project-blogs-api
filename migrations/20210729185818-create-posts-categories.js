44a5319494878cf4f922a3042ff910760af202d2
Lucas Silva <martssilvapro@gmail.com>
2021-07-29 16:26:01 -0300

    implementa migration categories

--- migrations/20210729185818-create-posts-categories.js
+++ migrations/20210729185818-create-posts-categories.js
@@ -2,20 +2,10 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    const postsCategories = queryInterface.createTable("")
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
   }
 };
--- migrations/20210729192007-categories.js
+++ migrations/20210729192007-categories.js
@@ -2,20 +2,23 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   const categories = queryInterface.createTable("Categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+   });
+
+   return categories;
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    queryInterface.dropTable("Categories");
   }
 };
