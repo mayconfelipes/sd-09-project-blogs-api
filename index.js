6c4541456614a2d6014cdd64a6531245a614d67b
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-07-29 18:00:59 -0300

    Variaveis de ambiente

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+const { PORT } = process.env;
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-console.log('Teste')
+app.listen(PORT || 3000, () => console.log(`ouvindo porta ${PORT || 3000}!`));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
