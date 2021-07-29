8a7caff9495602d24251f085cb581901dce07164
Douglas Ferreira <dodofp.df@gmail.com>
2021-07-29 16:04:54 -0300

    Criado as 4 migrations. Commit feito antes do db:migrate

---
+++ migrations/20210729183952-create-BlogPosts.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      number: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('BlogPosts');
+  },
+};
