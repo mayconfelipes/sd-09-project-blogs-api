40172154ce7050ad7646fa2cf898ccdc95410810
Douglas Ferreira <dodofp.df@gmail.com>
2021-07-29 16:40:52 -0300

    Feito model de blogpost, categorie e postscategorie

---
+++ models/postsCategorie.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostCategorie = sequelize.define('PostCategorie', 
+  {},
+  { timestamps: false });
+
+  PostCategorie.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Categorie, {
+      as: 'Categories',
+      through: PostCategorie,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Categorie.belongsToMany(models.BlogPost, {
+      as: 'BlogPosts',
+      through: PostCategorie,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+  return PostCategorie;
+};
