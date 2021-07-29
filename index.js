b7eec8315994c74d6f1d591ede1054bdffed03d6
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-29 18:39:01 -0300

    Get Users / Get Posts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const { Users, BlogPosts, Categories, PostsCategories } = require('./models');
 
 const app = express();
 // const config = require('./config/config');
-const dotenv = require('dotenv');
 
 dotenv.config();
 console.log(process.env.MYSQL_USER);
@@ -16,4 +17,26 @@ app.get('/', (request, response) => {
   response.send('Pai tá ON!!!');
 });
 
+app.get('/users', async (_req, res) => {
+  try {
+    const users = await Users.findAll();
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+});
+
+app.get('/post', async (_req, res) => {
+  try {
+    const posts = await BlogPosts.findAll();
+    return res.status(200).json(posts);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+});
 // app.post('/user', createUser);
+
+// npx sequelize-cli db:seed:all
+// npx sequelize-cli db:drop 
