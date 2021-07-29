9b5a49e8a076608120bc364db6a5d9910eaf3cec
LucioOSilva <lucioosilva00@gmail.com>
2021-07-29 13:28:40 -0300

    added users table migration

---
+++ models/users.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+    underscored: true,
+  });
+
+  return Users;
+};
