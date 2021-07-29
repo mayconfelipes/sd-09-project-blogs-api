97402ebdc9f8a33b2e27deb11313f24dcdbd3152
Lucas Silva <martssilvapro@gmail.com>
2021-07-29 16:47:32 -0300

    implementa migration posts categories

--- migrations/20210729185818-create-posts-categories.js
+++ migrations/20210729185818-create-posts-categories.js
@@ -2,7 +2,24 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const postsCategories = queryInterface.createTable("")
+    const postsCategories = queryInterface.createTable("PostsCategories", {
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "",
+          foreignKey: ""
+        },
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "",
+          foreignKey: ""
+        },
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
