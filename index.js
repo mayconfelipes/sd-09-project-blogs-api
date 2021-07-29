eea0466c842f93d0511a7c8a2cdad7cfc4f09c39
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-07-29 16:41:35 -0300

    Inicio do projeto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
+console.log('Teste')
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
