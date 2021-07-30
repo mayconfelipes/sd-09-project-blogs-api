1dcdcc45ee88b6ea5269aacb1d04ec143a870a02
Arthur de Sa <arthurdesa94@gmail.com>
2021-07-30 15:39:54 -0300

    vaiiiii

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 
-// again
+const {
+  UserController,
+  LoginController,
+  CategoriesController,
+  PostCategoriesController,
+} = require('./controllers');
 
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/user', UserController);
+
+app.use('/login', LoginController);
+
+app.use('/categories', CategoriesController);
+
+app.use('/post', PostCategoriesController);
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
