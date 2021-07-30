bee2310646b678772518ea6fb2f60d0ad2180a40
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-30 02:00:52 -0300

    rota user/:id

---
+++ middlewares/validationLogin.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+const validation = (body) => {
+  const bodyValidation = Joi.object().keys({
+    email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+    password: Joi.string().alphanum().length(6).required(),
+  });
+  const { error } = Joi.validate(body, bodyValidation);
+  if (error === null) {
+    return body;
+  } 
+    const { details } = error;
+    const message = details.map((i) => i.message).join(',');
+    return ({ error: message });
+};
+
+module.exports = validation;
