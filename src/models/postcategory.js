3044ddb5f6cf742d025eff87094452c88f141965
Matheus Martino <matheus.martino97@gmail.com>
2021-08-01 03:04:16 -0300

    Criando model postcategory.

---
+++ src/models/postcategory.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory', {}, { timestamps: false },
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
