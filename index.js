770d9be7f967f4fa4ee550a5b5a62928b0b55eb6
brenno-calado <brenno.calado@gmail.com>
2021-08-02 15:25:26 -0300

    add dotenv PORT

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
