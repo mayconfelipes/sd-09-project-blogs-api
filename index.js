df7749b37cc6fec5d271212b3dc359ef7cfec7a9
filipelouzano <filipe.louzano@idinheiro.com.br>
2021-08-01 21:56:02 -0300

    Ajuste index

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
