f600603d6932738c1fae2819511737d95537d9e9
Vagner Pereira <sousa.vagner82@gmail.com>
2021-08-02 17:07:20 -0300

    req 03

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
-const userController = require('./controllers/UserController');
-const loginController = require('./controllers/loginController');
+const { userController, loginController } = require('./controllers');
 const { error } = require('./middlewares');
 
 const app = express();
