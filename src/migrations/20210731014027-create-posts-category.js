33b7ca269f801dfe416a8ab5298583f4f10f0bd3
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-08-03 13:02:47 -0300

    estrutura sequelize criada

---
+++ src/migrations/20210731014027-create-posts-category.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      // id: {
+      //   allowNull: false,
+      //   autoIncrement: true,
+      //   primaryKey: true,
+      //   type: Sequelize.INTEGER,
+      // },
+      postId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  },
+};
---
+++ src/models/blogpost.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+  }, { timestamps: true });
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+  };
+  return BlogPost;
+};
