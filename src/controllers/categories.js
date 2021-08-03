9de1492915c909e6954f43b78ddd0a6ebd56106a
Felipe Cardoso <felipedclc@gmail.com>
2021-08-03 16:07:28 -0300

    arrumando controller categories

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,20 +1,20 @@
 const Joi = require('joi');
 const rescue = require('express-rescue');
-const { Category } = require('../models');
+const { Categories } = require('../models');
 const validate = require('../middlewares/validate');
 
 const create = [
   validate(Joi.object({ name: Joi.string().not().empty().required() })),
   rescue(async (req, res) => {
     const { name } = req.body;
-    const newCategory = await Category.create({ name });
+    const newCategory = await Categories.create({ name });
     
     return res.status(201).json(newCategory);
   }),
 ];
 
 const findAll = rescue(async (_req, res) => {
-  const categories = await Category.findAll();
+  const categories = await Categories.findAll();
   return res.status(200).json(categories);
 });
 
