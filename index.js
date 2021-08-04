0342e774d4f23a383cb45f28aa7ae0caf919c026
Carine Becker <caarine.m@gmail.com>
2021-08-04 14:09:04 -0300

    Feat: Parte do req. 1, cria token

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { createUser } = require('./controllers/user');
+const bodyParser = require('body-parser').json();
+
+const userControllers = require('./controllers/user');
+const { error, validateUser } = require('./middlewares/index');
 
 const app = express();
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
+app.use(bodyParser);
+app.use(error);
+app.post('/user', rescue(validateUser, userControllers.createUser));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.post('/user', rescue(createUser));
