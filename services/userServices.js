0055a53c9b633f2141893da24415d3fc225df151
Enio Nicael <enionicael@gmail.com>
2021-08-04 01:42:46 -0300

    Requisito 1, Implementacao da rota para cadastro de usuarios.

--- services/userServices.js
+++ services/userServices.js
@@ -1,14 +1,19 @@
 const { User } = require('../models');
 const { schema, validateError } = require('./schemas/userSchema');
-const { badRequest } = require('../helpers/getHttpStatusCode');
+const { badRequest, conflict } = require('../helpers/getHttpStatusCode');
 
 const createUser = async (userData) => {
-  const { image: _, ...body } = userData;
-  const { error } = schema.validate(body);
+  const { displayName, email, password } = userData;
+
+  const { error } = schema.validate({ displayName, email, password });
   if (error) throw validateError(badRequest, error.message);
 
+  const userEmail = await User.findOne({ email });
+
+  if (userEmail) throw validateError(conflict, 'User already registered');
+
   const newUser = await User.create(userData);
-  // console.log('[output] > ', newUser);
+
   return newUser;
 };
 
