99a9b3267ced25d32bbb22ce864a222412beaff8
Lucas Silva <martssilvapro@gmail.com>
2021-08-04 13:06:28 -0300

    corrige Post services

--- services/Post.js
+++ services/Post.js
@@ -15,18 +15,18 @@ const validateCategory = (categoryIds) => {
 const validateIfCategoriesExists = async (categoryIds) => {
   const categories = await Category.findAll({ where: { id: categoryIds } });
 
-  if (!categories) return { err: { status: 400, message: '"categoryIds" not found' } };
+  if (categories.length === 0) return { err: { status: 400, message: '"categoryIds" not found' } };
 };
 
 const createPost = async (userId, title, categoryIds, content) => {
   const validTitle = validateTitle(title);
   const validContent = validateContent(content);
+  const existsCategory = await validateIfCategoriesExists(categoryIds);
   const validCategory = validateCategory(categoryIds);
-  const existsCategory = validateIfCategoriesExists(categoryIds);
   if (validTitle) return validTitle.err;
   if (validContent) return validContent.err;
+  if (existsCategory) return existsCategory.err;
   if (validCategory) return validCategory.err;
-  if (await existsCategory) return existsCategory.err;
   const post = await BlogPost.create({ userId, title, categoryIds, content });
   return post;
 };
