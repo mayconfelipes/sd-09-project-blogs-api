b922565309d8f95615df1649e2379260f52e8c5c
felipe <felipesantiagolds@gmail.com>
2021-08-05 14:10:39 -0300

    .Config, .Env and Index updated

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use('./user', UserRouter);
+app.use('./login', UserRouter);
+app.use('./categories', UserRouter);
+app.use('./post', UserRouter);
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
