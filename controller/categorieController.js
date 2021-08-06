bcba9a76db2e6f8a6349eea88248235fee43b169
Rafael <rafaelsantanafernandes@gmail.com>
2021-08-06 00:58:27 -0300

    done six again

--- controller/categorieController.js
+++ controller/categorieController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Categorie } = require('../models');
 const { auth } = require('../middlewares/auth');
-const { BADREQUEST, INTERNERERROR, CREATE, OK } = require('../ultils');
+// const { BADREQUEST, INTERNERERROR, CREATE, OK } = require('../ultils');
 
 const router = express.Router();
 
@@ -9,14 +9,14 @@ router.post('/', auth, async (req, res) => {
     const { name } = req.body;
 
     if (!name) {
-        return res.status(BADREQUEST).json({ message: '"name" is required' });
+        return res.status(400).json({ message: '"name" is required' });
     }
     try {
         const newCategory = await Categorie.create({ name });
         
-        return res.status(CREATE).json(newCategory);
+        return res.status(201).json(newCategory);
     } catch (error) {
-        return res.status(INTERNERERROR).json(error);
+        return res.status(400).json(error);
     }
 });
 
@@ -24,9 +24,9 @@ router.get('/', auth, async (req, res) => {
     try {
         const categories = await Categorie.findAll();
         console.log(categories);
-        res.status(OK).json(categories);
+        res.status(200).json(categories);
     } catch (error) {
-        return res.status(INTERNERERROR).json(error);
+        return res.status(400).json(error);
     }
 });
 
