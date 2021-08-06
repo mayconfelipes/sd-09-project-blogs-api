89c85198498c546c4a371967413cd2b901fefe88
ViniciusChagas <viniciushrcs@gmail.com>
2021-08-06 11:41:25 -0300

    Criação das migrations e models [blogpost, categoy e user]

---
+++ src/models/blogpost.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: { type: DataTypes.DATE, defaultValue: Date.now() },
+    updated: { type: DataTypes.DATE, defaultValue: Date.now() },
+  }, { timestamps: false });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPost;
+};
