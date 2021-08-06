5cea606b1cbcddafe4c604515a9b41cbabff7de0
RodrigoNeoEx <draja.acc@gmail.com>
2021-08-06 12:40:32 -0300

    created userServices

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,1 +1,9 @@
-// const { User } = require('../models');
+const { User } = require('../models');
+
+const createUser = (user) => User.create(user);
+
+const getAllUsers = () => User.findAll();
+
+const getUserById = (id) => User.findByPk(id);
+
+module.exports = { createUser, getAllUsers, getUserById };
