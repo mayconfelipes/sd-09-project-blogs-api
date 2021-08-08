ede93abc70f62bcabe1e7b8b24824d507bf2235a
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-08-08 03:39:46 -0300

    [fix][lint]

--- routers/categoriesRouter.js
+++ routers/categoriesRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const rescue = require('express-rescue');
+// const rescue = require('express-rescue');
 // rescue se comporta como um try catch para capturar os erros e passar pro middleware de erros genericos
-const categoriesController = require('../controllers/categoriesController');
+// const categoriesController = require('../controllers/categoriesController');
 
 const categoriesRouter = express.Router();
 
-categoriesRouter.post('/', rescue(categoriesController.createcategory));
-categoriesRouter.get('/', rescue(categoriesController.getAllcategories));
+// categoriesRouter.post('/', rescue(categoriesController.createcategory));
+// categoriesRouter.get('/', rescue(categoriesController.getAllcategories));
 
 module.exports = categoriesRouter;
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const rescue = require('express-rescue');
+// const rescue = require('express-rescue');
 // rescue se comporta como um try catch para capturar os erros e passar pro middleware de erros genericos
-const postController = require('../controllers/postController');
+// const postController = require('../controllers/postController');
 
 const postRouter = express.Router();
 
-postRouter.post('/', rescue(postController.createPost));
-postRouter.get('/', rescue(postController.getAllPosts));
+// postRouter.post('/', rescue(postController.createPost));
+// postRouter.get('/', rescue(postController.getAllPosts));
 
 module.exports = postRouter;
