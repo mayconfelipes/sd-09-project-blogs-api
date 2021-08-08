03f101eb618867fbf52477d9a700b75d0de0e96a
julianafatsil <julianafatsil@gmail.com>
2021-08-08 20:42:42 -0300

    Created arrow function findByEmail

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,1 +1,8 @@
 const { User } = require('../models');
+
+// lint reclamou do await, imagino que como ele esta na mesma linha e é somente um
+const findByEmail = async (email) => User.findOne({ where: { email } });
+
+module.exports = { 
+  findByEmail,
+};
