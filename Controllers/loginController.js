67afe92a15c9a1c56cf22b4d70730c1910a9c5b9
felipe <felipesantiagolds@gmail.com>
2021-08-08 17:10:56 -0300

    Requirement 2

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -5,7 +5,6 @@ const LoginService = require('../Services/loginService');
 
 module.exports = rescue(async (req, res, _next) => {
   await LoginService(req.body);
-
   const token = JWT(req.body);
   res.status(200).json({ token });
 });
--- Services/loginService.js
+++ Services/loginService.js
@@ -1,10 +1,14 @@
 const Schema = require('../Utils/schemas');
 const ValidateError = require('../Utils/validateError');
+const { User } = require('../models');
 
 const validateLogin = async (loginData) => {
   const { error } = Schema.login.validate(loginData);
-
   if (error) throw ValidateError(400, error.message);
+
+  const user = await User.findOne({ where: { email: loginData.email } });
+  if (!user) throw ValidateError(400, 'Invalid fields');
+
   return true;
 };
 
