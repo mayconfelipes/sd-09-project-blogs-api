b9b3875a87f9f475f112fee0ab9e1444845d7191
pedrohrbispo <pedroenrique.400@gmail.com>
2021-08-09 11:09:15 -0300

    feature: req 7

--- models/postsCategories.js
+++ models/postsCategories.js
@@ -1,9 +1,22 @@
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, _DataTypes) => {
  const PostsCategories = sequelize.define(
-   'PostCategories',
-   {},
-   { timestamps: false },
+   'PostCategories', {}, { timestamps: false },
  ); 
 
+ PostsCategories.associate = (models) => {
+  models.Categories.belongsToMany(models.BlogPosts, {
+    as: 'blogsposts',
+    through: PostsCategories,
+    foreignKey: 'productId',
+    otherKey: 'selloffId',
+  });
+
+  models.Selloff.belongsToMany(models.Product, {
+    as: 'products',
+    through: SelloffProducts,
+    foreignKey: 'selloffId',
+    otherKey: 'productId',
+  });
+};
  return PostsCategories;
 };
