d4a29d24a2fcaaad0c5c05e8503d6e9109e9e5e7
muriloPereira006 <muriloedu006@gmail.com>
2021-08-09 13:03:00 -0300

    ok passou.. :/

--- controller/controllerCategory.js
+++ controller/controllerCategory.js
@@ -5,13 +5,16 @@ require('dotenv').config();
 const createCategory = async (req, res) => {
   try {
     const { name } = req.body;
-    const { token } = req.headers.authorization;
+    const token = req.headers.authorization;
+
+    console.log(token);
 
     if (!token) return res.status(401).json({ message: 'Token not found' });
     jwt.verify(token, process.env.JWT_SECRET);
-
-    const response = await Categories.create(name);
-    return res.status(201).json({ response });
+    
+    const response = await Categories.create({ name });
+    console.log(response);
+    return res.status(201).json(response.name);
   } catch (error) {
     return res.status(401).json({ message: 'Expired or invalid token' });
   }
