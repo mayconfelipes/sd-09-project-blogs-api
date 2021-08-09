20c46d09589ddcbd8eaa132cc4899afa835d23e4
Enio Nicael <enionicael@gmail.com>
2021-08-09 09:03:57 -0300

    Criacao da tabela BlogPosts.

---
+++ models/BlogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+  },
+  {
+    timestamps: true,
+    createdAt: 'published',
+    updatedAt: 'updated',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'users',
+    });
+  };
+
+  return BlogPost;
+};
--- models/User.js
+++ models/User.js
@@ -6,5 +6,11 @@ module.exports = (sequelize, DataTypes) => {
     image: DataTypes.STRING,
   }, { timestamps: false });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blogposts',
+    });
+  };
+
   return User;
 };
