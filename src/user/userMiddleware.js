4158ce1221083d6c833c18ed79a37c049d12cd1d
Renato Feitosa <pythaqua@gmail.com>
2021-08-09 01:35:42 -0300

    fix(userMiddleware): Set password length

--- src/user/userMiddleware.js
+++ src/user/userMiddleware.js
@@ -4,7 +4,7 @@ const validateUser = async (req, _res, next) => {
   const { error } = Joi.object({
     displayName: Joi.string().min(8).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().length(6).required(),
   }).validate(req.body);
   if (error) return next(error);
   next();
