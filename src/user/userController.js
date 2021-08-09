b0240f16f7361e249ce434421ac2ff5afa5218ae
Renato Feitosa <pythaqua@gmail.com>
2021-08-09 01:41:35 -0300

    fix(user): Change res code & password validation

--- src/user/userController.js
+++ src/user/userController.js
@@ -3,7 +3,7 @@ const { auth } = require('../utils');
 const create = async (req, res) => {
   const { displayName, email, password, image } = req.body;
   const token = auth.createToken({ displayName, email });
-  res.status(200).json({ token });
+  res.status(201).json({ token });
 };
 
 module.exports = {
--- src/user/userMiddleware.js
+++ src/user/userMiddleware.js
@@ -5,6 +5,7 @@ const validateUser = async (req, _res, next) => {
     displayName: Joi.string().min(8).required(),
     email: Joi.string().email().required(),
     password: Joi.string().length(6).required(),
+    image: Joi.string(),
   }).validate(req.body);
   if (error) return next(error);
   next();
