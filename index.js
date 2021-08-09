3fe41bf2dfa721c6ed77d20626fe4d24a04fa04f
Breno <blf@ic.ufal.br>
2021-08-09 19:33:14 -0300

    esqueci de salvar rs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/user', routes.usersRouter);
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
