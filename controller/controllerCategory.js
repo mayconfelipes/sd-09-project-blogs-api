dd65051348fdd96b823b828478280f9e6f22f045
muriloPereira006 <muriloedu006@gmail.com>
2021-08-09 13:03:22 -0300

    req 05

--- controller/controllerCategory.js
+++ controller/controllerCategory.js
@@ -7,14 +7,11 @@ const createCategory = async (req, res) => {
     const { name } = req.body;
     const token = req.headers.authorization;
 
-    console.log(token);
-
     if (!token) return res.status(401).json({ message: 'Token not found' });
     jwt.verify(token, process.env.JWT_SECRET);
     
     const response = await Categories.create({ name });
-    console.log(response);
-    return res.status(201).json(response.name);
+    return res.status(201).json(response);
   } catch (error) {
     return res.status(401).json({ message: 'Expired or invalid token' });
   }
