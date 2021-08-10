ae86e2ef48927b4b6a66324f8d46de69147c9dbb
Rafael <rafaelsantanafernandes@gmail.com>
2021-08-10 15:36:04 -0300

    done 9

--- controller/posts.js
+++ controller/posts.js
@@ -30,4 +30,45 @@ router.post('/', auth, validate.validatePost, async (req, res) => {
     return res.status(201).json(newBlogPost);
 });
 
+router.get('/:id', auth, async (req, res) => {
+  const { id } = req.params;
+
+  const post = await BlogPost.findOne(
+    { where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Categorie, as: 'categories', through: { attributes: [] } },
+    ] },
+  );
+
+  if (!post) {
+    return res.status(404).json({ message: 'Post does not exist' });
+  }
+
+  return res.status(200).json(post);
+});
+
+router.put('/:id', auth, validate.categoriesValidate, async (req, res) => {
+  const { id } = req.params;
+  const { email } = req.user; // variavel saldo do auth middleware
+  const { title: titles, content: contents } = req.body;
+
+  const postsId = await BlogPost.findOne({ where: { id }, include: ['user', 'categories'] });
+  // if (!postsId) {
+  //   return res.status(404).json({ message: 'not found' });
+  // }
+  const usersId = await User.findOne({ whre: { email } });
+  if (postsId.id !== usersId.id) {
+    return res.status(401).json({ message: 'Unauthorized user' });
+  }
+  await BlogPost.update({ titles, contents }, { where: { id } });
+  const update = await BlogPost.findOne({ where: { id }, include: ['user', 'categories'] }); 
+  console.log(update);
+  const { dataValues: { title, content, userId } } = update;
+  const { name } = update.categories[0];
+  // const { id } = update.categories[0];
+  
+  return res.status(200).json({ title, content, userId, categories: { name, id } });
+});
+
 module.exports = router;
--- middlewares/posts.js
+++ middlewares/posts.js
@@ -1,3 +1,4 @@
+// const Joi = require('joi');
 const { BADREQUEST } = require('../ultils');
 const { Categorie } = require('../models');
 
@@ -26,7 +27,6 @@ const validatePost = [
 
         const categories = await Categorie.findAll();
         const ids = categories.map((response) => response.id);
-        // console.log(ids); // verifica os ids
         
         for (let i = 0; i < categoryIds.length; i += 1) {
             if (!ids.includes(categoryIds[i])) {
@@ -38,6 +38,38 @@ const validatePost = [
     },
 ];
 
+const categoriesValidate = [
+  async (req, res, next) => {
+  // const schema = Joi.object({
+  //   title: Joi.string().required(),
+  //   content: Joi.string().required(),
+  // });
+  // const { error } = schema.validate(req.body);
+
+  // if (error) {
+  //   return res.status(400).json(error.message);
+  // }
+  // next();
+  const { title, content } = req.body;
+
+  if (!title) {
+      return res.status(400).json({ message: '"title" is required' });
+  }
+  if (!content) {
+    return res.status(400).json({ message: '"content" is required' });
+  }
+  next();
+},
+
+  async (req, res, next) => {
+    if (Object.keys(req.body).includes('categoryIds')) {
+      return res.status(400).json({ message: 'Categories cannot be edited' });
+    }
+    next();
+  },
+];
+
 module.exports = {
     validatePost,
+    categoriesValidate,
 };
