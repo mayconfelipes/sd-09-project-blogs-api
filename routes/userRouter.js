9100ac60fe12d2b77dbd92af13972c245960cf8b
Rafael <rafaeljavaro750@gmail.com>
2021-08-11 15:20:13 -0300

    Arruma Linter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,9 +5,7 @@ const { tokenGenerator, validateNewUserInput } = require('../middlewares/index')
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    User.findAll().then((data) => {
-      return res.status(200).json(data);
-    }).catch((e) => {
+    User.findAll().then((data) => res.status(200).json(data)).catch((e) => {
       console.log(e.message);
       return res.status(500).json({ message: 'Algo deu Errado!' });
     });
@@ -17,9 +15,8 @@ router.post('/', validateNewUserInput, (req, res) => {
   const { displayName, email, password, image } = req.body;
   const newUserInfo = { displayName, email, password, image };
 
-  User.create(newUserInfo).then(() => {
-    return res.status(201).send({ token: tokenGenerator(newUserInfo) });
-  }).catch((e) => {
+  User.create(newUserInfo)
+    .then(() => res.status(201).send({ token: tokenGenerator(newUserInfo) })).catch((e) => {
     console.log(e.message);
     return res.status(304).send({ message: 'Novo usuário não foi cadastrado. Algo deu Errado' });
   });
