5e196e1e1192dfd7990c016bc9d48d12bf6fd86a
Rafael <rafaeljavaro750@gmail.com>
2021-08-12 11:50:50 -0300

    Arruma validação CheckEmail - Req 1 Aprovado - Valida input EndPoint /user

--- middlewares/checkEmail.js
+++ middlewares/checkEmail.js
@@ -3,15 +3,16 @@ const { User } = require('../models/index');
 const checkEmail = async (req, res, next) => {
     const { email } = req.body;
     const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const userAlreadyExist = await User.findOne({ where: { email } });
 
-    if (!email || email === undefined) {
+    if (!email) {
       return res.status(400).send({ message: '"email" is required' });
     }
 
     if (!validEmail.test(email)) {
       return res.status(400).send({ message: '"email" must be a valid email' });
     }
+
+    const userAlreadyExist = await User.findOne({ where: { email } });
 
     if (userAlreadyExist) {
       return res.status(409).send({ message: 'User already registered' });
