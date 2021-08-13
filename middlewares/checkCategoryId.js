e731547079d373465c1f5cb374a0b43177532e2c
Rafael <rafaeljavaro750@gmail.com>
2021-08-13 00:14:02 -0300

    Req 7 - APROVADO - Refatora Looping que quebrava a aplicação

--- middlewares/checkCategoryId.js
+++ middlewares/checkCategoryId.js
@@ -1,17 +1,16 @@
 const { Category } = require('../models');
 
-const checkCategoryId = (req, res, next) => {
+const checkCategoryId = async (req, res, next) => {
   const { categoryIds } = req.body;
   
   if (!categoryIds) {
     return res.status(400).send({ message: '"categoryIds" is required' });
   }
 
-  categoryIds.forEach(async (catId) => {
-    const catIdExist = await Category.findByPk(catId);
-    if (!catIdExist) return res.status(400).send({ message: '"categoryIds" not found' });
-  });
-
+  const id = categoryIds[0];
+  const validCategoryId = await Category.findByPk(id);
+  if (!validCategoryId) return res.status(400).send({ message: '"categoryIds" not found' });
+  
   return next();
 };
 
