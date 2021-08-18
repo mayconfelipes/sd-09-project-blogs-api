f0922b95438a789523c9815a06cde78169c5c300
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-08-18 11:05:22 -0300

    Ajuste Req 2

--- services/user.js
+++ services/user.js
@@ -49,26 +49,22 @@ const validatePassword = async (req, res, next) => {
 
 const validateLoginEmail = async (req, res, next) => {
   const { email } = req.body;
-  const regex = /\S+@\S+\.\S+/;
-  if (email === '' || !email) {
-    return res.status(400).json({ message: '"email" is not allowed to be empty' });
+  if (!email) {
+    return res.status(400).json({ message: '"email" is required' });
   }
-  if (!regex.test(email)) {
-    return res.status(400).json({ message: '"email" must be a valid email' });
+  if (email === '') {
+    return res.status(400).json({ message: '"email" is not allowed to be empty' });
   }
   next();
 };
 
 const validateLoginPassword = async (req, res, next) => {
   const { password } = req.body;
-  if (password === '' || !password) {
-    return res.status(400).json({ message: '"password" is not allowed to be empty' });
+  if (!password) {
+    return res.status(400).json({ message: '"password" is required' });
   }
-  
-  if (password.length < 6) {
-    return res.status(400).json({ 
-      message: '"password" length must be 6 characters long',
-    });
+  if (password === '') {
+    return res.status(400).json({ message: '"password" is not allowed to be empty' });
   }
   next();
 };
