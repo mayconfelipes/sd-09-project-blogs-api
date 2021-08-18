06ab8129d6e5b638d8cd5eb8d997e7606bd6ceb1
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-08-18 09:21:42 -0300

    Ajuste Req 1

--- migrations/20210818045658-create-categories.js
+++ migrations/20210818045658-create-categories.js
@@ -11,14 +11,6 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
         allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
       }
     });
   },
