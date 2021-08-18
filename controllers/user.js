17be81b57237ecd73cd986efb842ec296919e342
Kevin Fraga <kevinciolac@gmail.com>
2021-08-18 04:42:48 -0300

    :one: POST /user fix

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,7 @@ const router = express.Router();
 const schema = joi.object({
   displayName: joi.string().min(8).required(),
   email: joi.string().email().required(),
-  password: joi.string().min(6).required(),
+  password: joi.string().length(6).required(),
   image: joi.string(),
 });
 
